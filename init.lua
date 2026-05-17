dfff791a71137caae80759e24ca0d3415f5d173e
MertzAndreas <Andreasmertz11@gmail.com>
2026-05-17 18:45:51 +0200

    Removed a bunch of plugins

--- init.lua
+++ init.lua
@@ -1,56 +1,45 @@
 vim.cmd.packadd "nvim.undotree"
 
 vim.pack.add({
-    -- Utils
-    { src = "https://www.github.com/nvim-lua/plenary.nvim", version = "master" },
+  -- Git
+  { src = "https://github.com/dlyongemallo/diffview.nvim" },
+  { src = "https://github.com/nvim-mini/mini.diff" },
+  { src = "https://github.com/nvim-mini/mini-git" },
 
-    -- Git
-    { src = "https://github.com/dlyongemallo/diffview.nvim" },
-    { src = "https://github.com/nvim-mini/mini.diff" },
-    { src = "https://github.com/nvim-mini/mini-git" },
+  -- Theme
+  { src = "https://github.com/RRethy/base16-nvim" },
 
-    -- Theme
-    { src = "https://github.com/RRethy/base16-nvim" },
+  -- UI
+  { src = "https://github.com/folke/todo-comments.nvim" },
+  { src = "https://github.com/folke/snacks.nvim" },
+  { src = "https://github.com/hat0uma/csvview.nvim" },
+  { src = "https://github.com/folke/which-key.nvim" },
+  { src = "https://github.com/nvim-lualine/lualine.nvim" },
+  { src = "https://github.com/MeanderingProgrammer/render-markdown.nvim" },
+  { src = "https://github.com/nvim-mini/mini.icons" },
 
-    -- UI
-    { src = "https://github.com/folke/todo-comments.nvim" },
-    { src = "https://github.com/folke/snacks.nvim" },
-    { src = "https://github.com/folke/which-key.nvim" },
-    { src = "https://github.com/nvim-lualine/lualine.nvim" },
-    { src = "https://github.com/nvim-tree/nvim-web-devicons" },
-    { src = "https://github.com/MeanderingProgrammer/render-markdown.nvim" },
-    { src = "https://github.com/nvim-mini/mini.icons" },
+  -- LSP
+  { src = "https://github.com/neovim/nvim-lspconfig" },
+  { src = "https://github.com/folke/lazydev.nvim" },
+  { src = "https://github.com/stevearc/conform.nvim" },
 
-    -- LSP
-    { src = "https://github.com/neovim/nvim-lspconfig" },
-    { src = "https://github.com/folke/lazydev.nvim" },
-    { src = "https://github.com/stevearc/conform.nvim" },
+  -- Editing
+  { src = "https://github.com/folke/flash.nvim" },
+  { src = "https://github.com/saghen/blink.cmp",                         version = vim.version.range "*" },
+  { src = "https://github.com/windwp/nvim-ts-autotag" },
+  { src = "https://github.com/nvim-mini/mini.surround" },
+  { src = "https://github.com/nvim-mini/mini.ai" },
+  { src = "https://github.com/nvim-mini/mini.pairs" },
+  { src = "https://github.com/folke/persistence.nvim" },
 
-    -- Editing
-    { src = "https://github.com/folke/flash.nvim" },
-    { src = "https://github.com/mertzandreas/roslyn-kit.nvim" },
-    { src = "https://github.com/saghen/blink.cmp", version = vim.version.range "*" },
-    { src = "https://github.com/windwp/nvim-ts-autotag" },
-    { src = "https://github.com/nvim-mini/mini.surround" },
-    { src = "https://github.com/nvim-mini/mini.ai" },
-    { src = "https://github.com/nvim-mini/mini.pairs" },
-    { src = "https://github.com/ThePrimeagen/harpoon", version = "harpoon2" },
-    { src = "https://github.com/chomosuke/typst-preview.nvim" },
-    { src = "https://github.com/hat0uma/csvview.nvim" },
-
-    -- Misc
-    { src = "https://github.com/nvim-treesitter/nvim-treesitter" },
-
-    -- AI
-    { src = "https://github.com/zbirenbaum/copilot.lua" },
-    { src = "https://www.github.com/olimorris/codecompanion.nvim", version = vim.version.range "^19.0.0" },
+  -- Misc
+  { src = "https://github.com/nvim-treesitter/nvim-treesitter" },
 })
 
 require "options"
 require "keymaps"
 require "autocmds"
 require "plugins.ui"
-require "plugins.ai"
 require "plugins.editing"
 require "plugins.git"
 require "plugins.lsp"
--- lua/plugins/editing.lua
+++ lua/plugins/editing.lua
@@ -1,13 +1,9 @@
 local keys = require("utils").keys
--- require("roslyn-kit").setup()
 require("nvim-ts-autotag").setup()
 require("mini.surround").setup()
 require("mini.ai").setup()
 require("mini.pairs").setup()
-require("typst-preview").setup({
-  -- Hack for dms.
-  open_cmd = 'dms open %s > /dev/null 2>&1',
-})
+
 keys({
   {
     "s",
@@ -27,70 +23,6 @@ keys({
   },
 })
 
-local harpoon = require "harpoon"
-harpoon:setup()
-
--- Jump directly
-for i = 1, 9 do
-  vim.keymap.set("n", "<leader>" .. i, function()
-    harpoon:list():select(i)
-  end, { desc = "Jump Harpoon " .. i })
-end
-
-vim.keymap.set("n", "<leader>a", function()
-  harpoon:list():add()
-end, { desc = "Harpoon file" })
-
-local function open_harpoon_picker()
-  local list = harpoon:list()
-  local items = {}
-  for i, h_item in ipairs(list.items) do
-    table.insert(items, {
-      idx = i,
-      text = h_item.value,
-      file = h_item.value,
-      harpoon_item = h_item,
-    })
-  end
-
-  Snacks.picker({
-    title = "Harpoon",
-    items = items,
-    format = "file",
-    confirm = function(picker, item)
-      picker:close()
-      -- Use the stored position to select correctly
-      list:select(item.idx)
-    end,
-    actions = {
-      harpoon_remove = function(picker)
-        local selected = picker:selected({ fallback = true })
-        if not selected or #selected == 0 then
-          return
-        end
-        for _, sel in ipairs(selected) do
-          list:remove(sel.harpoon_item)
-        end
-        picker:close()
-        vim.schedule(open_harpoon_picker)
-      end,
-    },
-    win = {
-      input = {
-        keys = {
-          ["<C-d>"] = { "harpoon_remove", mode = { "n", "i" } },
-        },
-      },
-      list = {
-        keys = {
-          ["d"] = { "harpoon_remove", mode = { "n" } },
-        },
-      },
-    },
-  })
-end
-
-vim.keymap.set("n", "<C-e>", open_harpoon_picker, { desc = "Harpoon menu" })
 require("csvview").setup({
   keymaps = {
     -- Text objects for selecting fields
@@ -146,3 +78,10 @@ vim.api.nvim_create_autocmd("BufReadCmd", {
     vim.fn.delete(tmp_csv)
   end,
 })
+
+
+require "persistence".setup()
+vim.keymap.set("n", "<leader>qs", function() require("persistence").load() end)
+vim.keymap.set("n", "<leader>qS", function() require("persistence").select() end)
+vim.keymap.set("n", "<leader>ql", function() require("persistence").load({ last = true }) end)
+vim.keymap.set("n", "<leader>qd", function() require("persistence").stop() end)

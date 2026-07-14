2a5fc1c43eeb02dd84858510ab92d02fc4ce10e1
MertzAndreas <Andreasmertz11@gmail.com>
2026-07-14 10:39:41 +0200

    Easy close vim.pack view

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -55,6 +55,7 @@ vim.api.nvim_create_autocmd("FileType", {
         "qf",
         "git",
         "nvim-undotree",
+        "nvim-pack",
     },
     callback = function(event)
         vim.bo[event.buf].buflisted = false
